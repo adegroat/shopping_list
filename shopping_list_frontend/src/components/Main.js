0144ac86a640ea157fc5426c3265d555cba2e9e4
Alex DeGroat <degroatalex@gmail.com>
2022-06-15 13:13:31 -0600

    Add loading indicator

--- shopping_list_frontend/src/components/Main.js
+++ shopping_list_frontend/src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, CircularProgress, Box } from '@mui/material';
 
 import ShoppingList from './ShoppingList';
 import ItemForm from './ItemForm';
@@ -7,12 +7,14 @@ import ItemForm from './ItemForm';
 const Main = () => {
   const [listItems, setListItems] = useState([]);
   const [addItemVisible, setAddItemVisible] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const loadItemList = () => {
     const fetchData = async () => {
       const data = await fetch('http://localhost:8080/api/list_items');
       const jsonData = await data.json();
       setListItems(jsonData);
+      setLoading(false);
     }
     fetchData().catch(console.log);
   }
@@ -23,19 +25,28 @@ const Main = () => {
 
   return(
     <>
-      <div className="flex-col">
-        {listItems.length > 0 && <ShoppingList loadItemList={loadItemList} items={listItems} />}
-        
-        {listItems.length <= 0 && (
-          <div className="empty-list">
-            <div>Your shopping list is currently empty :(</div>
-            
-            <div>
-              <Button onClick={() => setAddItemVisible(true)} variant="contained">Add Items</Button>
+      {loading && (
+        <Box sx={{display: 'flex', justifyContent: 'center', mt: '48px'}}>
+          <CircularProgress size={80}/>
+        </Box>
+      )}
+
+      {!loading && (
+        <div className="flex-col">
+          {listItems.length > 0 && <ShoppingList loadItemList={loadItemList} items={listItems} />}
+          
+          {listItems.length <= 0 && (
+            <div className="empty-list">
+              <div>Your shopping list is currently empty :(</div>
+              
+              <div>
+                <Button onClick={() => setAddItemVisible(true)} variant="contained">Add Items</Button>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
+
 
       {addItemVisible && (
         <ItemForm onClose={() => {
