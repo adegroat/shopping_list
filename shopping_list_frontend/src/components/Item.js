4a15c972c1ea54624fa07f0948feac689878d8c0
Alex DeGroat <degroatalex@gmail.com>
2022-06-15 06:26:34 -0600

    Work on main shopping list layout and item component

--- shopping_list_frontend/src/components/Item.js
+++ shopping_list_frontend/src/components/Item.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Item = ({name, description}) => {
+import { Box, Typography, Checkbox } from '@mui/material';
+
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+
+const Item = ({itemId, name, description, purchased}) => {
+  
+  const [purchasedChecked, setPurchasedChecked] = useState(purchased);  
+
+  const updatePurchased = (e) => {
+    setPurchasedChecked(!purchasedChecked);
+  }
+
+
   return (
-    <p>{name}</p>
+    <Box sx={{
+      display: 'flex', 
+      flexDirection: 'row', 
+      justifyContent: 'space-between', 
+      alignItems: 'center', 
+      border: 'solid 1px #ccc', 
+      mt: '8px', p: '8px'
+    }}>
+      <Box sx={{display: 'flex', flexDirection: 'row', flexGrow: 1, alignItems: 'center'}}>
+        <Box>
+          <Checkbox onChange={(e) => updatePurchased(e)} />
+        </Box>
+        
+        <Box sx={{display: 'flex', flexDirection: 'column', ml: '6px'}}>
+          <Typography sx={{
+            color: purchasedChecked ? '#1976d2' : 'black', 
+            fontWeight: 'bold', 
+            textDecoration: purchasedChecked ? 'line-through': 'normal'
+          }}>
+            {name}
+          </Typography>
+
+          <Typography color='gray' sx={{
+            textDecoration: purchasedChecked ? 'line-through': 'normal'
+          }}>
+            {description}
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box>
+        <EditOutlinedIcon />
+        <DeleteOutlinedIcon />
+      </Box>
+    </Box>
   )
 }
 
--- shopping_list_frontend/src/components/ShoppingList.js
+++ shopping_list_frontend/src/components/ShoppingList.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Grid } from '@mui/material';
+import { Container, Typography, Box, Button } from '@mui/material';
 
 import Item from './Item';
+import ItemForm from './ItemForm';
 
 const ShoppingList = ({items}) => {
+  const [itemFormVisible, setItemFormVisible] = useState(false);
+
   const itemsJsx = items.map(item => {
     return (
       <Item key={item.item_id} name={item.name} description={item.description} />
@@ -12,7 +15,22 @@ const ShoppingList = ({items}) => {
   });
 
   return(
-    <Grid>{itemsJsx}</Grid>
+    <>
+      <Container sx={{mt: '16px'}}>
+        <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+          <Typography variant='h6'>
+            Your Items
+          </Typography>
+
+          <Button onClick={() => setItemFormVisible(true)} variant="contained">Add Item</Button>
+        </Box>
+
+        {itemsJsx}
+      </Container>
+      
+      {itemFormVisible && <ItemForm onClose={() => setItemFormVisible(false)} />}
+
+    </>
   )
 }
 
